99e4dad2e035ab4f31d6c30ea440b827c90ec85c
ugurilgin <43095839+ugurilgin@users.noreply.github.com>
2022-09-01 12:37:38 +0300

    SpringBoot Initializr
    Spring Web,Spring Boot DevTools,Spring Data JPA,Validation,MySql Driver,Lombok,Spring Security

---
+++ src/main/java/com/ugisoftware/hotelmanagement/HotelmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.ugisoftware.hotelmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelmanagementApplication.class, args);
+	}
+
+}
